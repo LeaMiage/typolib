c3ddfe001bfd04bb1147a7649dde99533c28f677
LeaMiage <lea.cheoux@gmail.com>
2015-05-19 16:05:48 +0200

    suite

---
+++ app/views/rule_exceptionview.php
@@ -0,0 +1,14 @@
+<div class="exceptionview">
+
+    <fieldset id="exception_main">
+        <input type="hidden" name="id_rule" id="id_rule" value="1"/>
+        <fieldset>
+            <p>Enter a new exception:<br />
+            <input type="text" name="exception" id="exception"/></p>
+        </fieldset>
+
+        <br/>
+        <input type="button" id="submitRuleException" value="Add" alt="Add" />
+    </fieldset>
+
+</div>
